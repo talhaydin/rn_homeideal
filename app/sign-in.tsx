74159e72f0497e92eb138eb788a1ed3b4e9fe9ad
talhaaydin <mt.aydin@outlook.com>
2025-02-16 22:54:16 +0300

    login page

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,49 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
 
 const SignIn = () => {
+  const handleLogin = () => {};
+
   return (
-    <View>
-      <Text>sign-in</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="px-10 ">
+          <Text className="text-base text-center font-rubik text-black-200">
+            Welcome to Homeideal
+          </Text>
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's Get You Closer to {"\n"}
+            <Text className="text-primary-300">Your Ideal Home</Text>
+          </Text>
+          <Text className="text-lg font-rubik text-black-200 text-center mt-12">
+            Login to Homeideal with Google
+          </Text>
+          <TouchableOpacity
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
+            onPress={handleLogin}
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              ></Image>
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                Continue With Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
